63626a54703fa4a38f713fdc751b3d966973f3dd
Patrick Pichler <patri.pichler@gmail.com>
2018-11-04 16:57:03 +0100

    Fixed versions for reagent and re-frame

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject woshobiheitgsoffn2 "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.10.238"]
-                 [reagent "0.7.0"]
-                 [re-frame "0.10.5"]
+                 [reagent "0.8.1"]
+                 [re-frame "0.10.6"]
                  [cljsjs/material-ui "3.2.0-0"]
                  [cljsjs/material-ui-icons "3.0.1-0"]]
 
